4c457c5a997d0de2c734bebe2949efbd4e6da5ee
Selezen <a.a.selezenj@student.csn.khai.edu>
2021-10-14 20:19:09 +0300

    ChatBot {2-nd} stage

--- src/ChatBot.java
+++ src/ChatBot.java
@@ -1,3 +1,13 @@
-public class ChatBot {
+import java.util.Scanner;
 
+public class ChatBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Jon");
+        System.out.println("I was created in 2021");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, remind me your name.");
+        String name = scanner.nextLine();
+        System.out.println("What a great name you have, " + name);
+    }
 }
